3801811991dded5972da3879176aa01b16a18ae2
Jinmyeong Lee <jinious1111@naver.com>
2021-02-27 16:10:48 +0900

    model: Add Furniture class
    Signed-off-by: Jinmyeong Lee <jinious1111@naver.com>

---
+++ model/furniture.py
@@ -0,0 +1,12 @@
+
+class Furniture:
+	def __init__(self):
+		self.brand = ""
+		self.category = ""
+		self.price = ""
+		self.room = ""
+		self.location = (0, 0)
+		self.ref_url = ""
+		self.width = 0
+		self.height = 0
+		self.depth = 0
